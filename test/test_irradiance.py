75e90da06e00eaa03eed73899b5528877b631700
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2019-04-11 15:23:40 +0200

    create first tests for irradiance module

---
+++ test/test_irradiance.py
@@ -0,0 +1,58 @@
+from hydroshoot.data import potted_syrah, meteo
+from hydroshoot.irradiance import irradiance_distribution, hsCaribu, optical_prop
+from numpy.testing import assert_almost_equal
+
+
+def test_irradiance_distribution():
+    # sample values copied from data.json_parameters
+    location = (43.61, 3.87, 44.0)
+    e_type = 'Rg_Watt/m2'
+
+    # a particularly overcast day
+    day_met = meteo().iloc[:24,:]
+    sources, rdrs = irradiance_distribution(day_met, location, e_type)
+    assert rdrs == 1
+    # not optimals  no direct light is predicted for this day. Expect 46 instead
+    assert len(sources) == 70
+    nrj, pos = zip(*sources)
+    # bug : almost two fold diff between source energy sum and meteo Rg sum
+    assert_almost_equal(day_met.Rg.sum(),sum(nrj) ,0)
+
+    # a much clearer day
+    day_met = meteo().iloc[48:72,:]
+    sources, rdrs = irradiance_distribution(day_met, location, e_type)
+    # fail because of bug in irradiance : only last value of rdrs is returned
+    assert rdrs < 1
+    # not optimals
+    assert len(sources) == 70
+    # bug : almost two fold diff between source energy sum and meteo Rg sum
+    assert_almost_equal(day_met.Rg.sum(),sum(nrj) ,0)
+
+
+def test_hsCaribu():
+    g = potted_syrah()
+    # sample values copied from data.json_parameters
+    location = (43.61, 3.87, 44.0)
+    e_type = 'Rg_Watt/m2'
+    unit_scene_length = 'cm'
+
+    # reporduce preprocessing as done  in model.py
+    # Suppression of undesired geometry for light and energy calculations
+    geom_prop = g.properties()['geometry']
+    vidkeys = []
+    for vid in g.properties()['geometry']:
+        n = g.node(vid)
+        if not n.label.startswith(('L', 'other', 'soil')):
+            vidkeys.append(vid)
+    [geom_prop.pop(x) for x in vidkeys]
+    g.properties()['geometry'] = geom_prop
+    # Attaching optical properties to MTG elements
+    g = optical_prop(g)
+
+
+    # force sources
+    day_met = None
+    local_date = None
+    sources = [(1,(0,0,-1))]
+    # pattern = False (default) raises a bug
+    gg, cs = hsCaribu(g, day_met, local_date, location, e_type, unit_scene_length, source=sources, pattern=None)
