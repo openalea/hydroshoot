6f73b39c2cd1981911a1a3986effb88a10f6bb4e
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2019-05-09 19:01:18 +0200

    update test_energy.py add args to leaf_temperature function to force properties to a single user_defined scalar (for testing)

--- test/test_energy.py
+++ test/test_energy.py
@@ -1,31 +1,39 @@
 from non_regression_data import potted_syrah, meteo
-from hydroshoot.energy import pgl_scene, form_factors_simplified, leaf_temperature_init, leaf_temperature, forced_soil_temperature
+from hydroshoot.energy import form_factors_simplified, leaf_temperature, forced_soil_temperature
 from numpy.testing import assert_almost_equal
 import openalea.plantgl.all as pgl
+import hydroshoot.energy as energy
+import warnings
 
 
 def test_pgl_scene():
     g = potted_syrah()
     bc = pgl.BBoxComputer(pgl.Tesselator())
 
-    s = pgl_scene(g)
+    s = energy.pgl_scene(g)
     bc.process(s)
     bbox = bc.boundingbox
     zmin, zmax = bbox.getZMin(),bbox.getZMax()
     assert zmax > zmin > 0
 
-    s = pgl_scene(g, flip=True)
+    s = energy.pgl_scene(g, flip=True)
     bc.process(s)
     bbox = bc.boundingbox
     zmin, zmax = bbox.getZMin(),bbox.getZMax()
     assert 0 > zmax > zmin
 
     # check that original scene is still z >0
-    s = pgl_scene(g)
+    s = energy.pgl_scene(g)
     bc.process(s)
     bbox = bc.boundingbox
     zmin, zmax = bbox.getZMin(), bbox.getZMax()
     assert zmax > zmin > 0
+
+
+def test_get_leaves():
+    g = potted_syrah()
+    leaves = energy.get_leaves(g, leaf_lbl_prefix='L')
+    assert len(leaves) == 46
 
 
 def test_form_factors_simplified():
@@ -41,15 +49,36 @@ def test_form_factors_simplified():
     assert_almost_equal(sum(g.property('k_leaves').values()), 147.7, 1)
 
 
+def test_heat_boundary_layer_conductance():
+    g = potted_syrah()
+    met = meteo().iloc[[12], :]
+    gbH = energy.heat_boundary_layer_conductance(g, met)
+    assert len(gbH) == 46
+    assert_almost_equal(sum(gbH.values()) / len(gbH), 47, 0)
+
+
 def test_forced_soil_temperature():
     met = meteo().iloc[[12], :]
     tsoil = forced_soil_temperature(met)
     assert tsoil == met.Tac[0] + 20
+
 
 def test_leaf_temperature():
     g = potted_syrah()
     met = meteo().iloc[[12], :]
     tsoil = 20
     tsky = 2
-    g = leaf_temperature_init(g)
-    it = leaf_temperature(g, met, tsoil, tsky)
+
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('default')
+        tleaf, it = leaf_temperature(g, met, tsoil, tsky)
+        assert len(w) == 1
+        assert len(tleaf) == 46
+        for vid in tleaf:
+            assert tleaf[vid] == met.Tac[0]
+
+    g.properties()['gbH'] = energy.heat_boundary_layer_conductance(g, met)
+    tleaf, it = leaf_temperature(g, met, tsoil, tsky, k_sky=0.5, k_soil=0.5, k_leaves=1, Ei=0, E=0)
+    assert len(tleaf) == 46
+    for vid in tleaf:
+        assert tleaf[vid] != met.Tac[0]
