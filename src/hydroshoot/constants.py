f4095f9e2ae35a4828e5e7540ff03fd1c172f3d5
RamiALBASHA <rami.albacha@yahoo.com>
2021-10-19 11:16:41 +0200

    added rho and g_p to constants.py

--- src/hydroshoot/constants.py
+++ src/hydroshoot/constants.py
@@ -2,3 +2,12 @@ water_molar_mass = 18.01528
 """[g mol-1] Molar mass of pure water
 """
 
+water_density = 997.0479
+"""density of liquid water at 25 °C temperature [kg m-3]
+"""
+
+gravitational_acceleration = 9.81
+"""gravitational acceleration [m s-2]
+"""
+
+
