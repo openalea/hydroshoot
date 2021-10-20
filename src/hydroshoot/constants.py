5209a50d136aa2d6534b84b22ba1c1295811a473
RamiALBASHA <rami.albacha@yahoo.com>
2021-10-20 09:47:01 +0200

    mv constants into constants.py

--- src/hydroshoot/constants.py
+++ src/hydroshoot/constants.py
@@ -10,6 +10,30 @@ gravitational_acceleration = 9.81
 """gravitational acceleration [m s-2]
 """
 
+a_glob = 0.6
+"""[-] Lumped leaf absorptance in the shortwave band.
+"""
+
+e_sky = 1.0
+"""[-] Emissivity of the sky.
+"""
+
+e_leaf = 0.96
+"""[-] Emissivity of the leaf.
+"""
+
+e_soil = 0.95
+"""[-] Emissivity of the soil.
+"""
+
+sigma = 5.670373e-8
+"""[W m−2 K−4] Stefan–Boltzmann constant. 
+"""
+
+lambda_ = 44.0e3
+"""[W s mol−1] Latent heat for vapourization
+"""
+
 oxygen_partial_pressure = 210
 """[mmol mol-1]Oxygen partial pressure
 """
