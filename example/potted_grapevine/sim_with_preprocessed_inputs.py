f223381b5790875293fa75a1c8a82963ad6d2d3e
RamiALBASHA <rami.albacha@yahoo.com>
2023-01-09 10:57:46 +0100

    mkdir for preprocessed example

--- example/potted_grapevine/sim_with_preprocessed_inputs.py
+++ example/potted_grapevine/sim_with_preprocessed_inputs.py
@@ -14,14 +14,22 @@ from hydroshoot import io, model, initialisation
 
 def preprocess_inputs(grapevine_mtg: MTG, path_project_dir: Path, psi_soil: float, gdd_since_budbreak: float,
                       scene: Scene):
+    path_preprocessed_inputs = path_project_dir / 'preprocessed_inputs'
+    path_preprocessed_inputs.mkdir(parents=True, exist_ok=True)
+
     inputs = io.HydroShootInputs(g=grapevine_mtg, path_project=path_project_dir, scene=scene, psi_soil=psi_soil,
                                  gdd_since_budbreak=gdd_since_budbreak)
     io.verify_inputs(g=grapevine_mtg, inputs=inputs)
     grapevine_mtg = initialisation.init_model(g=grapevine_mtg, inputs=inputs)
 
+    print("Computing 'static' data...")
     static_data = {'form_factors': {s: grapevine_mtg.property(s) for s in ('ff_sky', 'ff_leaves', 'ff_soil')}}
     static_data.update({'Na': grapevine_mtg.property('Na')})
+    with open(path_preprocessed_inputs / 'static.json', mode='w') as f_prop:
+        dump(static_data, f_prop)
+    pass
 
+    print("Computing 'dynamic' data...")
     dynamic_data = {}
     inputs_hourly = io.HydroShootHourlyInputs(psi_soil=inputs.psi_soil_forced, sun2scene=inputs.sun2scene)
     for date_sim in inputs.params.simulation.date_range:
@@ -38,10 +46,6 @@ def preprocess_inputs(grapevine_mtg: MTG, path_project_dir: Path, psi_soil: floa
             'Ei': grapevine_mtg.property('Ei'),
             'Eabs': grapevine_mtg.property('Eabs')}})
 
-    path_preprocessed_inputs = path_project_dir / 'preprocessed_inputs'
-    with open(path_preprocessed_inputs / 'static.json', mode='w') as f_prop:
-        dump(static_data, f_prop)
-    pass
     with open(path_preprocessed_inputs / 'dynamic.json', mode='w') as f_prop:
         dump(dynamic_data, f_prop)
     pass
