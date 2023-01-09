6e6bd46e9122c467d303302df2b2a5b5cc2c88ed
RamiALBASHA <rami.albacha@yahoo.com>
2023-01-09 14:55:31 +0100

    bugfix grapevine mtg build of multiple plants

--- test/test_architecture.py
+++ test/test_architecture.py
@@ -6,6 +6,12 @@ Source_Dir = join(dirname(__file__), 'data')
 
 
 def test_cordon():
-    """Returns an `openalea.mtg` representing a potted syrah grapevine."""
     g = architecture.vine_mtg(join(Source_Dir, 'digit_vsp.csv'))
     architecture.cordon_vector(g=g)
+
+
+def test_multiple_plants_digit():
+    try:
+        architecture.vine_mtg(file_path=join(Source_Dir, 'digit_twin_pots.csv'))
+    except ValueError:
+        pass
