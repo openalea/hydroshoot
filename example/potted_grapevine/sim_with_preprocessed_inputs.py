cd9c99f0f0f35d27bce9a940377fda6ec6c8f8db
RamiALBASHA <rami.albacha@yahoo.com>
2023-04-07 10:28:02 +0200

    update dynamic and static preprocessed data for potted grapevine

--- example/potted_grapevine/sim_with_preprocessed_inputs.py
+++ example/potted_grapevine/sim_with_preprocessed_inputs.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     path_preprocessed_data = path_project / 'preprocessed_inputs'
 
     g, scene = build_mtg(path_file=path_project / 'grapevine_pot.csv', is_show_scene=False)
-    # preprocess_inputs(grapevine_mtg=g, path_project_dir=path_project, psi_soil=-0.2, gdd_since_budbreak=1000., scene=scene)
+    # preprocess_inputs(grapevine_mtg=g, path_project_dir=path_project, psi_soil=-0.2, gdd_since_budbreak=100., scene=scene)
 
     with open(path_preprocessed_data / 'static.json') as f:
         static_inputs = load(f)
