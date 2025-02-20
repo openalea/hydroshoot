1ba515f8ad21fca104564c98e1ca87eeaef6e17d
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-02-20 15:41:47 +0100

    Taking care of warnings

--- src/hydroshoot/preprocess.py
+++ src/hydroshoot/preprocess.py
@@ -7,7 +7,7 @@ from hydroshoot.utilities import calc_effective_daily_temperature
 
 def calc_gdd_since_budbreak(weather: DataFrame, date_budbreak: datetime, date_beg_sim: datetime,
                             temperature_base: float) -> float:
-    time_range = date_range(date_budbreak, date_beg_sim, freq='H')
+    time_range = date_range(date_budbreak, date_beg_sim, freq='h')
     df = weather.loc[time_range, :].resample('D').agg({'Tac': ['max', 'min']}).mean(axis=1)
     return df.apply(lambda x:
                     calc_effective_daily_temperature(temperature_air=x, temperature_base=temperature_base)).cumsum()[-1]
